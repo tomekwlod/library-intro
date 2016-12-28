eee7d23c611df2ef678c2e6ca7be2a69f623c83a
Tomek Wlodarczyk <twl@phase-ii.com>
2016-12-28 20:56:05 +0000

    Server plus simple tpl

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type Page struct {
+	Name string
+}
+
+func main() {
+	templates := template.Must(template.ParseFiles("templates/index.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		p := Page{Name: "Tomek"}
+		if name := r.FormValue("name"); name != "" {
+			p.Name = name
+		}
+
+		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	fmt.Println(http.ListenAndServe(":8080", nil))
+}
